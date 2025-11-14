e950219f46de2f670c8a9f78e820a3697249cd87
JSRobertOuimet <jsrobertouimet@gmail.com>
2025-11-14 09:07:36 -0500

    Prevented indexing.

--- src/includes/head.php
+++ src/includes/head.php
@@ -3,6 +3,9 @@
 <meta name="description" content="My name is Jean-Simon Robert Ouimet, and I am a passionate visual designer with a strong inclination towards front-end web development. I like to translate business requirements into seamless experiences informed by user research. I love to turn those experiences into elegant interfaces using the latest technologies. I enjoy both working independently or as part of a team where cooperation is the common denominator. My mother tongue is French and I’m proudly bilingual. I’m a former musician who persistently strives to create that pitch-perfect moment that will make a positive change for my audience.">
 <meta name="viewport" content="width=device-width, initial-scale=1">
 
+<!-- Preventing indexing -->
+<meta name="robots" content="noindex, nofollow" />
+
 <!--Favicon-->
 <link rel="shortcut icon" href="/assets/favicon/favicon.ico" type="image/x-icon">
 <link rel="apple-touch-icon" sizes="180x180" href="/assets/favicon/apple-touch-icon.png">
